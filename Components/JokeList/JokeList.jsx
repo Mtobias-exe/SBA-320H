5f0b70a949ef92cf660158dd897a56c075e65c32
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-25 00:19:06 -0500

    added button and picture

---
+++ Components/JokeList/JokeList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function JokeList() {
+  return (
+    <div className='Joke-cont'>
+        <img src='../public/neutral-face-removebg-preview.png' alt="dad neutral face"/>
+        <h2>Wanna hear a joke from your old man?</h2>
+        <button>Get New Joke</button>
+    
+    </div>
+
+  )
+}
+
+export default JokeList
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import JokeList from '../Components/JokeList/JokeList'
 
 function App() {
   return (
     <>
       <h1>Corny Dad Simulator</h1>
+      <JokeList></JokeList>
     </>
   )
 }
