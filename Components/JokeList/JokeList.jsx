1dc7fd4040c682c0e477af2fda83771b4afb6c87
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-25 22:50:49 -0500

    added an intial question, but the joke still appears

--- Components/JokeList/JokeList.jsx
+++ Components/JokeList/JokeList.jsx
@@ -5,18 +5,21 @@ function JokeList() {
   const [joke, setJoke] = useState([]);
   const [error, setError] = useState(null)
   const [headText, setText] = useState('Want to hear a joke from your old man?')
+  const [initialLoad, setInitialLoad] = useState(true);
   const URL = 'https://icanhazdadjoke.com/'
 
     const fetchJoke = async () => {
       try{
         const response = await axios.get(URL, { headers: { Accept: "application/json" } });
         setJoke(response.data);
-        setText('Want to hear another one?')
+        if (!initialLoad){
+          setText('Want to hear another one?')
+        }
+        setInitialLoad(false)
         } catch (err){
           setError(err.message);
         }
     }
-
     // wanna hear a joke from your old man? (img is neutral)
     // Would you like to hear another joke (img src is neutral )
     //  if yes fetch joke (img src dad laughing)
@@ -27,15 +30,11 @@ function JokeList() {
 
    }
 
-    
     useEffect(()=>{
       fetchJoke();
     }, []);
     
-
-
   if (error) console.log(`Error: ${error}`)
-
 
   return (
     <div className='Joke-cont'>
