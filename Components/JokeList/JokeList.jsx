5ab88d1bc87da20608fe623384e53bbd68cea6df
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-25 21:17:21 -0500

    working on fetching the api

--- Components/JokeList/JokeList.jsx
+++ Components/JokeList/JokeList.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 function JokeList() {
+  const [joke, setJoke] = useState([]);
+  const [error, setError] = useState(null)
+  const URL = 'https://icanhazdadjoke.com/'
+  
+  useEffect(() => {
+    const fetchJoke = async () => {
+      try{
+        const response = await axios.get(URL, { headers: { Accept: "application/json" } });
+        setJoke(response.data);
+        } catch (err){
+          setError(err.message);
+        }
+    }
+    fetchJoke();
+  }, [])
+
+  if (error) console.log(`Error: ${error}`)
+
+
   return (
     <div className='Joke-cont'>
         <img src='../public/neutral-face-removebg-preview.png' alt="dad neutral face"/>
         <h2>Wanna hear a joke from your old man?</h2>
-        <button>Get New Joke</button>
+        
+        {/* Joke here  */}
+        <p>{joke.value}</p>
+        <button className='btn' onClick={fetchJoke}>Get New Joke</button>
     
     </div>
 
