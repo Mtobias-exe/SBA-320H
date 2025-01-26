f216e46dfcfeda6225863c43c5185b2b2156bb74
Mtobias-exe <maryfrance.tobias@gmail.com>
2025-01-25 22:40:42 -0500

    trying to make dialogue more interactive

--- Components/JokeList/JokeList.jsx
+++ Components/JokeList/JokeList.jsx
@@ -4,17 +4,29 @@ import axios from 'axios';
 function JokeList() {
   const [joke, setJoke] = useState([]);
   const [error, setError] = useState(null)
+  const [headText, setText] = useState('Want to hear a joke from your old man?')
   const URL = 'https://icanhazdadjoke.com/'
-  
-  
+
     const fetchJoke = async () => {
       try{
         const response = await axios.get(URL, { headers: { Accept: "application/json" } });
         setJoke(response.data);
+        setText('Want to hear another one?')
         } catch (err){
           setError(err.message);
         }
     }
+
+    // wanna hear a joke from your old man? (img is neutral)
+    // Would you like to hear another joke (img src is neutral )
+    //  if yes fetch joke (img src dad laughing)
+    //  if no stop
+
+   const handleClick = () => {
+    fetchJoke()
+
+   }
+
     
     useEffect(()=>{
       fetchJoke();
@@ -28,11 +40,12 @@ function JokeList() {
   return (
     <div className='Joke-cont'>
         <img src='../public/neutral-face-removebg-preview.png' alt="dad neutral face"/>
-        <h2>Wanna hear a joke from your old man?</h2>
+        {/* <h2>Wanna hear a joke from your old man?</h2> */}
+        <h2>{headText}</h2>
         
         {/* Joke here  */}
         <p>{joke.joke}</p>
-        <button className='btn' onClick={fetchJoke}>Get New Joke</button>
+        <button className='btn' onClick={handleClick}>Get New Joke</button>
     
     </div>
 
